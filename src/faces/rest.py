f2a0ac87dc59e50fa7ec6d5eaa740e989f00d23c
thongchaic <thongchai.c@srru.ac.th>
2022-02-10 18:45:38 +0700

    RESTFul face

--- src/faces/rest.py
+++ src/faces/rest.py
@@ -1,8 +1,11 @@
 import requests 
 from ndn import Ndn 
 class RESTFul:
-    def __init__(self):
-        pass 
+    def __init__(self, uri):
+        self.URI = uri
+        self.onRecievedInterest = None
+        self.onReceivedData = None  
+
     
     def send(self,_type,name,payload):
         if _type == Ndn.DATA:
@@ -10,16 +13,20 @@ class RESTFul:
             return 
         #Interest pkt received 
         
+
     def receive(self):
         #Data pkt received 
 
-    def get(self,data):
-        pass
+    def get(self,params): #paprams format: x=1&y=2.5
+        rst = requests.get(self.URI+"?"+params)
+        return rst 
+
     def post(self):
         pass
     def put(self):
         pass
     def delete(self, id):
-        pass 
-         
+        pass
+    
+
     
